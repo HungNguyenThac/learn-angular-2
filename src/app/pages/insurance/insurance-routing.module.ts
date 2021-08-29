fa2b18d349eb3a880e734fa526cf7069a38fd9a6
Nguyễn Tiến Đạt <datnt@toprate.io>
2021-08-30 02:40:24 +0700

    insurance confirm information screen

--- src/app/pages/insurance/insurance-routing.module.ts
+++ src/app/pages/insurance/insurance-routing.module.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DetermineNeedsComponent } from './determine-needs/determine-needs.component';
 import { ChargeInsuranceComponent } from './charge-insurance/charge-insurance.component';
 import { InsuranceProductsChoicesComponent } from './insurance-products-choices/insurance-products-choices.component';
+import {ConfirmInformationComponent} from "./confirm-information/confirm-information.component";
 
 export const InsuranceRoutes: Routes = [
   {
@@ -9,23 +10,23 @@ export const InsuranceRoutes: Routes = [
     children: [
       {
         path: '',
-        children: [
-          {
-            path: '',
-            component: DetermineNeedsComponent,
-            data: { animation: true },
-          },
-          {
-            path: 'charge-insurance',
-            component: ChargeInsuranceComponent,
-            data: { animation: true },
-          },
-          {
-            path: 'insurance-choices',
-            component: InsuranceProductsChoicesComponent,
-            data: { animation: true },
-          },
-        ],
+        component: DetermineNeedsComponent,
+        data: { animation: true },
+      },
+      {
+        path: 'charge-insurance',
+        component: ChargeInsuranceComponent,
+        data: { animation: true },
+      },
+      {
+        path: 'confirm-information',
+        component: ConfirmInformationComponent,
+        data: { animation: true },
+      },
+      {
+        path: 'insurance-choices',
+        component: InsuranceProductsChoicesComponent,
+        data: { animation: true },
       },
     ],
   },
