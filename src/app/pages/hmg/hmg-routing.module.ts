cd3505c7eb6c4d824fa6222cd58f4428236274f7
minhdungdau <buiminhhp92@gmail.com>
2021-09-08 10:00:39 +0000

    UI form thông tin cá nhân và ký hợp đồng

--- src/app/pages/hmg/hmg-routing.module.ts
+++ src/app/pages/hmg/hmg-routing.module.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { ApprovalLetterSignComponent } from './approval-letter-sign/approval-letter-sign.component';
 import { CompaniesListComponent } from './companies-list/companies-list.component';
 import { IntroduceComponent } from './introduce/introduce.component';
+import { UserInfoFormComponent } from './user-info-form/user-info-form.component';
 
 export const HmgRoutes: Routes = [
   {
@@ -19,6 +21,16 @@ export const HmgRoutes: Routes = [
       {
         path: 'companies',
         component: CompaniesListComponent,
+        data: { animation: true },
+      },
+      {
+        path: 'user-info',
+        component: UserInfoFormComponent,
+        data: { animation: true },
+      },
+      {
+        path: 'approval-letter',
+        component: ApprovalLetterSignComponent,
         data: { animation: true },
       },
     ],
--- src/app/pages/hmg/hmg.module.ts
+++ src/app/pages/hmg/hmg.module.ts
@@ -6,16 +6,22 @@ import {RouterModule} from '@angular/router';
 import {SharedModule} from "../../share/shared.module";
 import { IntroduceComponent } from './introduce/introduce.component';
 import { CompaniesListComponent } from './companies-list/companies-list.component';
+import { UserInfoFormComponent } from './user-info-form/user-info-form.component';
+import { ApprovalLetterSignComponent } from './approval-letter-sign/approval-letter-sign.component';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @NgModule({
   declarations: [
     IntroduceComponent,
     CompaniesListComponent,
+    UserInfoFormComponent,
+    ApprovalLetterSignComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(HmgRoutes),
-    SharedModule
+    SharedModule,
+    PdfViewerModule,
   ]
 })
 export class HmgModule {
