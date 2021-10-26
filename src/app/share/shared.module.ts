40c933a27d86383e86d70ac4132297d30febac1b
Nguyễn Tiến Đạt <datnt@toprate.io>
2021-10-27 02:42:09 +0700

    css table dashboard

--- src/app/share/shared.module.ts
+++ src/app/share/shared.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MaterialModule } from './modules/material.modules';
-import { TranslateModule } from '@ngx-translate/core';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MaterialModule} from './modules/material.modules';
+import {TranslateModule} from '@ngx-translate/core';
 
 import * as fromComponents from './components';
 import * as fromValidators from './validators';
 import * as fromPipes from './pipes';
 import * as fromDirectives from './directives';
-import { FormsModule } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {FormsModule} from '@angular/forms';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -26,8 +26,9 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
     ...fromPipes.pipes,
   ],
   providers: [
-    { provide: MAT_DIALOG_DATA, useValue: {} },
-    { provide: MatDialogRef, useValue: {} },
+    {provide: MAT_DIALOG_DATA, useValue: {}},
+    {provide: MatDialogRef, useValue: {}},
   ],
 })
-export class SharedModule {}
+export class SharedModule {
+}
