d8f2b695eb2595aaa24e80959ae68999bb242619
Nguyễn Tiến Đạt <datnt@toprate.io>
2021-10-28 17:12:36 +0700

    customer list data

--- src/app/core/common/enum/operator.ts
+++ src/app/core/common/enum/operator.ts
@@ -12,3 +12,29 @@ export enum DOCUMENT_BTN_TYPE {
   DOWNLOAD = 'DOWNLOAD',
   DELETE = 'DELETE',
 }
+
+export enum QUERY_CONDITION_TYPE {
+  GREATER_THAN = '__gt',
+  GREATER_THAN_OR_EQUAL = '__ge',
+  LESS_THAN = '__lt',
+  LESS_THAN_OR_EQUAL = '__le',
+  BETWEEN = '__bw',
+  START_WITH = '__sw',
+  END_WITH = '__ew',
+  NOT_EQUAL = '__ne',
+  EQUAL = '__e',
+  IN = '__in',
+  LIKE = '__~',
+}
+
+export enum DATA_CELL_TYPE {
+  TEXT = 'TEXT',
+  DATETIME = 'DATETIME',
+  STATUS = 'STATUS',
+}
+
+export enum DATA_STATUS_TYPE {
+  PL_UI_STATUS = 'PL_UI_STATUS',
+  PL_HMG_STATUS = 'PL_HMG_STATUS',
+  PL_TNG_STATUS = 'PL_TNG_STATUS',
+}
---
+++ src/app/public/models/breadcrumb-options.model.ts
@@ -0,0 +1,11 @@
+export interface BreadcrumbOptionsModel {
+  title: string;
+  iconClass?: string;
+  iconImgSrc?: string;
+  searchPlaceholder?: string;
+  searchFields?: string[];
+  searchable?: boolean;
+  extraActionLabel?: string;
+  btnAddText?: string;
+  showBtnAdd?: boolean;
+}
