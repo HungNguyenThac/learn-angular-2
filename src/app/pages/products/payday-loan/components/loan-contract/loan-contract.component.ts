fdaec5d418dc774de7e0a97df710b2558cb5ea1f
tmq <taminhquan99@gmail.com>
2021-11-10 10:16:10 +0700

    get customer loan contract

--- src/app/pages/products/payday-loan/components/loan-contract/loan-contract.component.ts
+++ src/app/pages/products/payday-loan/components/loan-contract/loan-contract.component.ts
@@ -1,13 +1,15 @@
-import { MultiLanguageService } from 'src/app/share/translate/multiLanguageService';
-import { DomSanitizer } from '@angular/platform-browser';
-import { Component, Input, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ToastrService } from 'ngx-toastr';
-import { PaydayLoan } from 'open-api-modules/loanapp-api-docs';
+import {MultiLanguageService} from 'src/app/share/translate/multiLanguageService';
+import {DomSanitizer} from '@angular/platform-browser';
+import {Component, Input, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {ToastrService} from 'ngx-toastr';
+import {PaydayLoan} from 'open-api-modules/loanapp-api-docs';
 import {
   DATA_CELL_TYPE,
   DATA_STATUS_TYPE,
 } from 'src/app/core/common/enum/operator';
+import {LoanListService} from '../../loan-list/loan-list.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-loan-contract',
@@ -15,7 +17,26 @@ import {
   styleUrls: ['./loan-contract.component.scss'],
 })
 export class LoanContractComponent implements OnInit {
+  loanContractView: any;
+  loanContractData: any;
+  loanContractFile: any;
+  enableSign: boolean = false;
+  displayStatus;
+  downloadable: boolean = false;
+
+  subManager = new Subscription();
+
+  constructor(
+    private notifier: ToastrService,
+    private dialog: MatDialog,
+    private domSanitizer: DomSanitizer,
+    private multiLanguageService: MultiLanguageService,
+    private LoanListService: LoanListService
+  ) {
+  }
+
   _loanId: string;
+
   @Input()
   get loanId(): string {
     return this._loanId;
@@ -26,6 +47,7 @@ export class LoanContractComponent implements OnInit {
   }
 
   _loanDetail: PaydayLoan;
+
   @Input()
   get loanDetail(): PaydayLoan {
     return this._loanDetail;
@@ -36,20 +58,9 @@ export class LoanContractComponent implements OnInit {
     this.getDisplayStatus();
   }
 
-  loanContractView: any;
-  loanContractData: any;
-  contractStatus: string;
-  loanContractFile: any;
-  enableSign: boolean = false;
-  displayStatus;
-  constructor(
-    private notifier: ToastrService,
-    private dialog: MatDialog,
-    private domSanitizer: DomSanitizer,
-    private multiLanguageService: MultiLanguageService
-  ) {}
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._getLoanContractData();
+  }
 
   getDisplayStatus() {
     this.displayStatus = {
@@ -61,5 +72,76 @@ export class LoanContractComponent implements OnInit {
       format: DATA_STATUS_TYPE.PL_HMG_STATUS,
     };
     return this.displayStatus;
+  }
+
+  checkSignable() {
+    if (this.loanDetail.status === 'CONTRACT_AWAITING') {
+      this.enableSign = true;
+    }
+  }
+
+  onClickSign() {
+    const customerId = this.loanDetail.customerId;
+    const idRequest = this.loanContractData.idRequest;
+    const idDocument = this.loanContractData.idDocument;
+
+    this.subManager.add(
+      this.LoanListService.signContract(
+        customerId,
+        idRequest,
+        idDocument
+      ).subscribe((result) => {
+        console.log(result);
+      })
+    );
+  }
+
+  downloadFileContract(documentPath, customerId) {
+    this.subManager.add(
+      this.LoanListService.downloadSingleFileContract(
+        documentPath,
+        customerId
+      ).subscribe((data) => {
+        this.loanContractFile = data;
+        this.pdfView(this.loanContractFile);
+      })
+    );
+  }
+
+  onClickDownload() {
+    this.LoanListService.downloadBlobFile(this.loanContractFile);
+  }
+
+  pdfView(pdfurl: string) {
+    pdfurl += '#toolbar=0&navpanes=0&scrollbar=0&zoom=90';
+    this.loanContractView = this.domSanitizer.bypassSecurityTrustHtml(
+      "<iframe  src='" +
+      pdfurl +
+      "' type='application/pdf' style='width:100%; height: 70vh; background-color:white;'>" +
+      'Object ' +
+      pdfurl +
+      ' failed' +
+      '</iframe>'
+    );
+  }
+
+  private _getLoanContractData() {
+    this.subManager.add(
+      this.LoanListService.getContractData(
+        this.loanDetail.id,
+        this.loanDetail.customerId
+      ).subscribe((response) => {
+        if (response.result === null) {
+          return (this.loanContractData = 'no-contract');
+        }
+        this.downloadable = true;
+        this.loanContractData = response.result;
+        this.downloadFileContract(
+          this.loanContractData.path,
+          this.loanDetail.customerId
+        );
+        this.checkSignable();
+      })
+    );
   }
 }
--- src/app/pages/products/payday-loan/loan-list/loan-list.service.ts
+++ src/app/pages/products/payday-loan/loan-list/loan-list.service.ts
@@ -3,9 +3,16 @@ import { ApplicationTngControllerService } from '../../../../../../open-api-modu
 import { ApplicationHmgControllerService } from '../../../../../../open-api-modules/dashboard-api-docs';
 import { PaydayLoanControllerService } from '../../../../../../open-api-modules/loanapp-hmg-api-docs';
 import { Injectable } from '@angular/core';
+import { catchError, map } from 'rxjs/operators';
 import { CustomerControllerService } from 'open-api-modules/dashboard-api-docs';
 import * as _ from 'lodash';
 import { QUERY_CONDITION_TYPE } from '../../../../core/common/enum/operator';
+import {
+  ApiResponseContract,
+  ContractControllerService,
+} from '../../../../../../open-api-modules/loanapp-api-docs';
+import { FileControllerService } from '../../../../../../open-api-modules/com-api-docs';
+import { SignDocumentControllerService } from '../../../../../../open-api-modules/contract-api-docs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +22,10 @@ export class LoanListService {
     private paydayLoanControllerService: PaydayLoanControllerService,
     private applicationTngControllerService: ApplicationTngControllerService,
     private applicationHmgControllerService: ApplicationHmgControllerService,
-    private customerControllerService: CustomerControllerService
+    private customerControllerService: CustomerControllerService,
+    private contractControllerService: ContractControllerService,
+    private signContractAutomation: SignDocumentControllerService,
+    private fileControllerService: FileControllerService
   ) {}
 
   public getLoanDataHmg(params) {
@@ -105,5 +115,69 @@ export class LoanListService {
       params.orderBy,
       params.sortDirection === 'desc'
     );
+  }
+
+  public getContractData(loanId: string, customerId: string) {
+    return this.contractControllerService
+      .getActivePaydayLoan2(loanId, customerId)
+      .pipe(
+        map((results: ApiResponseContract) => {
+          console.log('display ok');
+          return results;
+        }),
+
+        catchError((err) => {
+          throw err;
+        })
+      );
+  }
+
+  public signContract(
+    customerId: string,
+    idRequest: number,
+    idDocument: number
+  ) {
+    return this.signContractAutomation
+      .v1SignAdminSignContractPost({ customerId, idRequest, idDocument })
+      .pipe(
+        map((results) => {
+          return results;
+        }),
+
+        catchError((err) => {
+          throw err;
+        })
+      );
+  }
+
+  public downloadSingleFileContract(documentPath: string, customerId: string) {
+    return this.fileControllerService
+      .downloadFile({ documentPath, customerId })
+      .pipe(
+        map((results) => {
+          return this.convertBlobType(results, 'application/pdf');
+        }),
+
+        catchError((err) => {
+          throw err;
+        })
+      );
+  }
+
+  public downloadBlobFile(src) {
+    const a = document.createElement('a');
+    a.setAttribute('target', '_blank');
+    a.setAttribute('href', src);
+    a.setAttribute('download', src.split('/').pop());
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    console.log(src);
+  }
+
+  public convertBlobType(data: any, type: string) {
+    let blob = new Blob([data], { type: type });
+    let url = window.URL.createObjectURL(blob);
+    return url;
   }
 }
